1d26b20e953e5e3f94fa761bbf635f751b84166d
liminghao <liminghao@xiaomi.com>
2014-09-22 22:08:30 +0800

    replace table to 4 spaces and fix set_base bug.

--- 0.11/sources/linux-mdf/include/linux/sched.h
+++ 0.11/sources/linux-mdf/include/linux/sched.h
@@ -16,11 +16,11 @@
 #error "Currently the close-on-exec-flags are in one word, max 32 files/proc"
 #endif
 
-#define TASK_RUNNING		0
-#define TASK_INTERRUPTIBLE	1
-#define TASK_UNINTERRUPTIBLE	2
-#define TASK_ZOMBIE		3
-#define TASK_STOPPED		4
+#define TASK_RUNNING        0
+#define TASK_INTERRUPTIBLE    1
+#define TASK_UNINTERRUPTIBLE    2
+#define TASK_ZOMBIE        3
+#define TASK_STOPPED        4
 
 #ifndef NULL
 #define NULL ((void *) 0)
@@ -38,99 +38,100 @@ extern int tty_write(unsigned minor,char * buf,int count);
 typedef int (*fn_ptr)();
 
 struct i387_struct {
-	long	cwd;
-	long	swd;
-	long	twd;
-	long	fip;
-	long	fcs;
-	long	foo;
-	long	fos;
-	long	st_space[20];	/* 8*10 bytes for each FP-reg = 80 bytes */
+    long    cwd;
+    long    swd;
+    long    twd;
+    long    fip;
+    long    fcs;
+    long    foo;
+    long    fos;
+    long    st_space[20];    /* 8*10 bytes for each FP-reg = 80 bytes */
 };
 
 struct tss_struct {
-	long	back_link;	/* 16 high bits zero */
-	long	esp0;
-	long	ss0;		/* 16 high bits zero */
-	long	esp1;
-	long	ss1;		/* 16 high bits zero */
-	long	esp2;
-	long	ss2;		/* 16 high bits zero */
-	long	cr3;
-	long	eip;
-	long	eflags;
-	long	eax,ecx,edx,ebx;
-	long	esp;
-	long	ebp;
-	long	esi;
-	long	edi;
-	long	es;		/* 16 high bits zero */
-	long	cs;		/* 16 high bits zero */
-	long	ss;		/* 16 high bits zero */
-	long	ds;		/* 16 high bits zero */
-	long	fs;		/* 16 high bits zero */
-	long	gs;		/* 16 high bits zero */
-	long	ldt;		/* 16 high bits zero */
-	long	trace_bitmap;	/* bits: trace 0, bitmap 16-31 */
-	struct i387_struct i387;
+    long    back_link;    /* 16 high bits zero */
+    long    esp0;
+    long    ss0;        /* 16 high bits zero */
+    long    esp1;
+    long    ss1;        /* 16 high bits zero */
+    long    esp2;
+    long    ss2;        /* 16 high bits zero */
+    long    cr3;
+    long    eip;
+    long    eflags;
+    long    eax,ecx,edx,ebx;
+    long    esp;
+    long    ebp;
+    long    esi;
+    long    edi;
+    long    es;        /* 16 high bits zero */
+    long    cs;        /* 16 high bits zero */
+    long    ss;        /* 16 high bits zero */
+    long    ds;        /* 16 high bits zero */
+    long    fs;        /* 16 high bits zero */
+    long    gs;        /* 16 high bits zero */
+    long    ldt;        /* 16 high bits zero */
+    long    trace_bitmap;    /* bits: trace 0, bitmap 16-31 */
+    struct i387_struct i387;
 };
 
 struct task_struct {
 /* these are hardcoded - don't touch */
-	long state;	/* -1 unrunnable, 0 runnable, >0 stopped */
-	long counter;
-	long priority;
-	long signal;
-	struct sigaction sigaction[32];
-	long blocked;	/* bitmap of masked signals */
+    long state;    /* -1 unrunnable, 0 runnable, >0 stopped */
+    long counter;
+    long priority;
+    long signal;
+    struct sigaction sigaction[32];
+    long blocked;    /* bitmap of masked signals */
 /* various fields */
-	int exit_code;
-	unsigned long start_code,end_code,end_data,brk,start_stack;
-	long pid,father,pgrp,session,leader;
-	unsigned short uid,euid,suid;
-	unsigned short gid,egid,sgid;
-	long alarm;
-	long utime,stime,cutime,cstime,start_time;
-	unsigned short used_math;
+    int exit_code;
+    unsigned long start_code,end_code,end_data,brk,start_stack;
+    long pid,father,pgrp,session,leader;
+    unsigned short uid,euid,suid;
+    unsigned short gid,egid,sgid;
+    long alarm;
+    long utime,stime,cutime,cstime,start_time;
+    unsigned short used_math;
 /* file system info */
-	int tty;		/* -1 if no tty, so it must be signed */
-	unsigned short umask;
-	struct m_inode * pwd;
-	struct m_inode * root;
-	struct m_inode * executable;
-	unsigned long close_on_exec;
-	struct file * filp[NR_OPEN];
+    int tty;        /* -1 if no tty, so it must be signed */
+    unsigned short umask;
+    struct m_inode * pwd;
+    struct m_inode * root;
+    struct m_inode * executable;
+    unsigned long close_on_exec;
+    struct file * filp[NR_OPEN];
 /* ldt for this task 0 - zero 1 - cs 2 - ds&ss */
-	struct desc_struct ldt[3];
+    struct desc_struct ldt[3];
 /* tss for this task */
-	struct tss_struct tss;
+    struct tss_struct tss;
 };
 
 /*
  *  INIT_TASK is used to set up the first task table, touch at
  * your own risk!. Base=0, limit=0x9ffff (=640kB)
  */
-#define INIT_TASK \
-/* state etc */	{ 0,15,15, \
-/* signals */	0,{{},},0, \
-/* ec,brk... */	0,0,0,0,0,0, \
-/* pid etc.. */	0,-1,0,0,0, \
-/* uid etc */	0,0,0,0,0,0, \
-/* alarm */	0,0,0,0,0,0, \
-/* math */	0, \
-/* fs info */	-1,0022,NULL,NULL,NULL,0, \
-/* filp */	{NULL,}, \
-	{ \
-		{0,0}, \
-/* ldt */	{0x9f,0xc0fa00}, \
-		{0x9f,0xc0f200}, \
-	}, \
-/*tss*/	{0,PAGE_SIZE+(long)&init_task,0x10,0,0,0,0,(long)&pg_dir,\
-	 0,0,0,0,0,0,0,0, \
-	 0,0,0x17,0x17,0x17,0x17,0x17,0x17, \
-	 _LDT(0),0x80000000, \
-		{} \
-	}, \
+#define INIT_TASK   \
+    { 0,   15, 15,  \
+      0, {{},}, 0,  \
+      0,0,0,0,0,0,  \
+      0,-1,0,0,0,   \
+      0,0,0,0,0,0,  \
+      0,0,0,0,0,0,  \
+      0,            \
+     -1,0022,NULL,NULL,NULL,0, \
+     {NULL,},       \
+     { \
+         {0,0},           \
+         {0x9f,0xc0fa00}, \
+         {0x9f,0xc0f200}, \
+     }, \
+     { \
+         0,PAGE_SIZE+(long)&init_task,0x10,0,0,0,0,(long)&pg_dir, \
+         0,0,0,0,0,0,0,0,                                         \
+         0,0,0x17,0x17,0x17,0x17,0x17,0x17,                       \
+         _LDT(0),0x80000000,                                      \
+         {}                                                       \
+     }, \
 }
 
 extern struct task_struct *task[NR_TASKS];
@@ -158,12 +159,12 @@ extern void wake_up(struct task_struct ** p);
 #define lldt(n) __asm__("lldt %%ax"::"a" (_LDT(n)))
 #define str(n) \
 __asm__("str %%ax\n\t" \
-	"subl %2,%%eax\n\t" \
-	"shrl $4,%%eax" \
-	:"=a" (n) \
-	:"a" (0),"i" (FIRST_TSS_ENTRY<<3))
+    "subl %2,%%eax\n\t" \
+    "shrl $4,%%eax" \
+    :"=a" (n) \
+    :"a" (0),"i" (FIRST_TSS_ENTRY<<3))
 /*
- *	switch_to(n) should switch tasks to task nr n, first
+ *    switch_to(n) should switch tasks to task nr n, first
  * checking that n isn't the current task, in which case it does nothing.
  * This also clears the TS-flag if the task we switched to has used
  * tha math co-processor latest.
@@ -171,42 +172,44 @@ __asm__("str %%ax\n\t" \
 #define switch_to(n) {\
 struct {long a,b;} __tmp; \
 __asm__("cmpl %%ecx,current\n\t" \
-	"je 1f\n\t" \
-	"movw %%dx,%1\n\t" \
-	"xchgl %%ecx,current\n\t" \
-	"ljmp %0\n\t" \
-	"cmpl %%ecx,last_task_used_math\n\t" \
-	"jne 1f\n\t" \
-	"clts\n" \
-	"1:" \
-	::"m" (*&__tmp.a),"m" (*&__tmp.b), \
-	"d" (_TSS(n)),"c" ((long) task[n])); \
+    "je 1f\n\t" \
+    "movw %%dx,%1\n\t" \
+    "xchgl %%ecx,current\n\t" \
+    "ljmp %0\n\t" \
+    "cmpl %%ecx,last_task_used_math\n\t" \
+    "jne 1f\n\t" \
+    "clts\n" \
+    "1:" \
+    ::"m" (*&__tmp.a),"m" (*&__tmp.b), \
+    "d" (_TSS(n)),"c" ((long) task[n])); \
 }
 
 #define PAGE_ALIGN(n) (((n)+0xfff)&0xfffff000)
 
 #define _set_base(addr,base) \
-__asm__("movw %%dx,%0\n\t" \
-	"rorl $16,%%edx\n\t" \
-	"movb %%dl,%1\n\t" \
-	"movb %%dh,%2" \
-	::"m" (*((addr)+2)), \
-	  "m" (*((addr)+4)), \
-	  "m" (*((addr)+7)), \
-	  "d" (base) \
-	)
+__asm__("pushl %%edx\n\t" \
+    "movw %%dx,%0\n\t"       \
+    "rorl $16,%%edx\n\t" \
+    "movb %%dl,%1\n\t" \
+    "movb %%dh,%2" \
+    "pop %%edx"      \
+    ::"m" (*((addr)+2)), \
+      "m" (*((addr)+4)), \
+      "m" (*((addr)+7)), \
+      "d" (base) \
+    )
 
 #define _set_limit(addr,limit) \
 __asm__("movw %%dx,%0\n\t" \
-	"rorl $16,%%edx\n\t" \
-	"movb %1,%%dh\n\t" \
-	"andb $0xf0,%%dh\n\t" \
-	"orb %%dh,%%dl\n\t" \
-	"movb %%dl,%1" \
-	::"m" (*(addr)), \
-	  "m" (*((addr)+6)), \
-	  "d" (limit) \
-	)
+    "rorl $16,%%edx\n\t" \
+    "movb %1,%%dh\n\t" \
+    "andb $0xf0,%%dh\n\t" \
+    "orb %%dh,%%dl\n\t" \
+    "movb %%dl,%1" \
+    ::"m" (*(addr)), \
+      "m" (*((addr)+6)), \
+      "d" (limit) \
+    )
 
 #define set_base(ldt,base) _set_base( ((char *)&(ldt)) , base )
 #define set_limit(ldt,limit) _set_limit( ((char *)&(ldt)) , (limit-1)>>12 )
@@ -214,13 +217,13 @@ __asm__("movw %%dx,%0\n\t" \
 #define _get_base(addr) ({\
 unsigned long __base; \
 __asm__("movb %3,%%dh\n\t" \
-	"movb %2,%%dl\n\t" \
-	"shll $16,%%edx\n\t" \
-	"movw %1,%%dx" \
-	:"=d" (__base) \
-	:"m" (*((addr)+2)), \
-	 "m" (*((addr)+4)), \
-	 "m" (*((addr)+7))); \
+    "movb %2,%%dl\n\t" \
+    "shll $16,%%edx\n\t" \
+    "movw %1,%%dx" \
+    :"=d" (__base) \
+    :"m" (*((addr)+2)), \
+     "m" (*((addr)+4)), \
+     "m" (*((addr)+7))); \
 __base;})
 
 #define get_base(ldt) _get_base( ((char *)&(ldt)) )
