b4a69f92964f78bbe6d027014050cae802a4f971
liminghao <liminghao@xiaomi.com>
2014-10-14 22:07:55 +0800

    I'm cause a mistake.
    N/A
    
    caanot fix bug: We don't support separate I&D
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- 0.11/sources/linux-mdf/include/asm/system.h
+++ 0.11/sources/linux-mdf/include/asm/system.h
@@ -1,19 +1,17 @@
 #define move_to_user_mode() \
-__asm__ ("pushl %%eax\n\t" \
-        "movl %%esp,%%eax\n\t"    \
+__asm__ ("movl %%esp,%%eax\n\t"    \
 	"pushl $0x17\n\t" \
 	"pushl %%eax\n\t" \
 	"pushfl\n\t" \
 	"pushl $0x0f\n\t" \
 	"pushl $1f\n\t" \
-        "pop %%eax\n\t" \
 	"iret\n" \
 	"1:\tmovl $0x17,%%eax\n\t" \
 	"movw %%ax,%%ds\n\t" \
 	"movw %%ax,%%es\n\t" \
 	"movw %%ax,%%fs\n\t" \
 	"movw %%ax,%%gs" \
-	::)
+         :::"ax")
 
 #define sti() __asm__ ("sti"::)
 #define cli() __asm__ ("cli"::)
